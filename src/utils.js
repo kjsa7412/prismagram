34fbb4e6c6691b17a8934c8a33f8ae210b1f60be
Kyungjin Sa <48097915+kjsa7412@users.noreply.github.com>
2019-04-02 03:52:12 +0900

    #3.6 Passport JWT part Three

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,23 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
 import { adjectives, nouns } from "./words";
 import sgMail from "@sendgrid/mail";
-// import nodemailer from "nodemailer";
-// import sgTransport from "nodemailer-sendgrid-transport";
+import jwt from "jsonwebtoken";
+
+console.log(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
 
-console.log(process.env.SENDGRID_API_KEY);
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 export const sendSecretMail = (address, secret) => {
   const email = {
     from: "kyungjin@prismagram.com",
     to: address,
     subject: "🔒Login Secret for Prismagram🔒",
-    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`
+    html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/website to log in`
   };
   return sgMail.send(email);
 };
+
+export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
